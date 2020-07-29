5f51be501a6e3dd40a1c9cb00a2cd799951d81f1
mikel-js <michaelcastro.js@gmail.com>
2020-07-29 23:40:03 +0300

    added some files

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -11,9 +11,29 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   return (
     <div className='profile-github'>
       <h2 className='text-primary my-1'>Github Repos</h2>
-      {repos === null ? <SPinner /> : (
+      {repos === null ? <Spinner /> : (
         repos.map((repo) => (
-          <div></div>
+          <div key={repo._id} className='repo bg-white p-1 my-1'>
+            <div>
+              <h4>
+                <a href={repo.html_url} target='_blank' rel='noopener noreferer'>{repo.name}</a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className='badge badge-primary'>
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className='badge badge-dark'>
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className='badge badge-light'>
+                  Forks: {repo.forks_count}
+                </li>
+              </ul>
+            </div>
+          </div>
         ))
       )}
     </div>
