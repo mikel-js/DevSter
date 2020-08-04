532d106c4a074ed4478c61776d99be90b1ae5393
mikel-js <michaelcastro.js@gmail.com>
2020-08-04 14:18:13 +0300

    Deployment preparations

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,8 +13,7 @@ const Landing = ({ isAuthenticated }) => {
         <div className="landing-inner">
           <h1 className="x-large">DevSter</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            Create a developer portfolio, share posts and connect with other developers!
         </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">Sign Up</Link>
