eb253a03bd8de4b0a09381fd892850f9899fa50a
mikel-js <michaelcastro.js@gmail.com>
2020-08-04 14:04:47 +0300

    Deployment preparation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,15 +11,22 @@ connectDB()
 // Initialize middleware-to get data from User.js req.body
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
 
 // Define routes. yung /api/users e yung sa http un
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.length('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
